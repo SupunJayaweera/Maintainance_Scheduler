30bd1310c48f8a05036a45aaf45fc7e1d7662a49
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2025-08-04 21:06:08 +0530

    installed nodemailer and replace SendGrid

--- backend/libs/send-email.js
+++ backend/libs/send-email.js
@@ -1,21 +1,55 @@
-import sGmail from "@sendgrid/mail";
+// import sGmail from "@sendgrid/mail";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// sGmail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// const fromEmail = process.env.FROM_EMAIL;
+
+// export const sendEmail = async (to, subject, html) => {
+//   try {
+//     const msg = {
+//       to,
+//       from: `Maintenance App <${fromEmail}>`,
+//       subject,
+//       html,
+//     };
+//     await sGmail.send(msg);
+//     console.log("Email sent successfully");
+//     return true;
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     return false;
+//   }
+// };
+
+// export default sendEmail;
+
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-sGmail.setApiKey(process.env.SENDGRID_API_KEY);
-
-const fromEmail = process.env.FROM_EMAIL;
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: process.env.EMAIL_SECURE === "true",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 export const sendEmail = async (to, subject, html) => {
   try {
-    const msg = {
+    const mailOptions = {
+      from: process.env.EMAIL_FROM,
       to,
-      from: `Maintenance App <${fromEmail}>`,
       subject,
       html,
     };
-    await sGmail.send(msg);
+    await transporter.sendMail(mailOptions);
     console.log("Email sent successfully");
     return true;
   } catch (error) {
