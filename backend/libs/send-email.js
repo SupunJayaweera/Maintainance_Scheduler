56a32b8f64ce6d55885d59e56fab9298fea4f7fb
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2025-10-26 12:48:58 +0530

    switched the email sending option to sengrid

--- backend/libs/send-email.js
+++ backend/libs/send-email.js
@@ -1,55 +1,21 @@
-// import sGmail from "@sendgrid/mail";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// sGmail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// const fromEmail = process.env.FROM_EMAIL;
-
-// export const sendEmail = async (to, subject, html) => {
-//   try {
-//     const msg = {
-//       to,
-//       from: `Maintenance App <${fromEmail}>`,
-//       subject,
-//       html,
-//     };
-//     await sGmail.send(msg);
-//     console.log("Email sent successfully");
-//     return true;
-//   } catch (error) {
-//     console.error("Error sending email:", error);
-//     return false;
-//   }
-// };
-
-// export default sendEmail;
-
-import nodemailer from "nodemailer";
+import sGmail from "@sendgrid/mail";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_SECURE === "true",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+sGmail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const fromEmail = process.env.FROM_EMAIL;
 
 export const sendEmail = async (to, subject, html) => {
   try {
-    const mailOptions = {
-      from: process.env.EMAIL_FROM,
+    const msg = {
       to,
+      from: `Maintenance App <${fromEmail}>`,
       subject,
       html,
     };
-    await transporter.sendMail(mailOptions);
+    await sGmail.send(msg);
     console.log("Email sent successfully");
     return true;
   } catch (error) {
@@ -59,3 +25,37 @@ export const sendEmail = async (to, subject, html) => {
 };
 
 export default sendEmail;
+
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: process.env.EMAIL_PORT,
+//   secure: process.env.EMAIL_SECURE === "true",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export const sendEmail = async (to, subject, html) => {
+//   try {
+//     const mailOptions = {
+//       from: process.env.EMAIL_FROM,
+//       to,
+//       subject,
+//       html,
+//     };
+//     await transporter.sendMail(mailOptions);
+//     console.log("Email sent successfully");
+//     return true;
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     return false;
+//   }
+// };
+
+// export default sendEmail;
